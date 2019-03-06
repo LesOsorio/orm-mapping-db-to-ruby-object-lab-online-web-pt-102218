9cff592ad4264460adbe3e266cfcd2cc6e1a3e73
Leslie Osorio <lesosorio17@gmail.com>
2019-03-06 00:44:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -118,7 +118,7 @@ class Student
       WHERE students.grade = 'X'
     SQL
 
-    DB[:conn]execute(sql).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end 
