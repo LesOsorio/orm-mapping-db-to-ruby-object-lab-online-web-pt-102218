9887047821b3bb13f6935630a7f31223ca6a086b
Leslie Osorio <lesosorio17@gmail.com>
2019-03-06 00:26:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -73,7 +73,7 @@ class Student
     end
   end
 
-  def self.all_students_below_12th_grade
+  def self.students_below_12th_grade
     sql = <<-SQL
       SELECT *
       FROM students
@@ -83,7 +83,7 @@ class Student
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
-  end 
+  end
 
 
 end
