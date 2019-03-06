4a4407472660658e35ff8fade3356f1b4377bd36
Leslie Osorio <lesosorio17@gmail.com>
2019-03-06 00:26:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -77,7 +77,7 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE students.grade = < 12
+      WHERE students.grade = <12
     SQL
 
     DB[:conn].execute(sql).map do |row|
