174a62d5d9d0ecb81e5da80748a10404e1bf5ca2
Leslie Osorio <lesosorio17@gmail.com>
2019-03-06 00:38:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -102,7 +102,7 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE new_students.id = 2
+      WHERE new_student.id = 2
     SQL
 
     DB[:conn].execute(sql).map do |row|
